38df44d954ceff742e92319a9d5d240bb5bf4d3e
schase <schase18@yahoo.com>
2021-04-12 23:14:09 -0400

    Added if statement

--- main.py
+++ main.py
@@ -17,5 +17,14 @@ words = ['I', 'love', 'Python']
 for w in words:
     print(w, len(w))
 
+x = int(input("Please enter an integer:"))
+if x < 0:
+    print("negative")
+elif x == 0:
+    print("Zero")
+elif x == 1:
+    print("One")
+else:
+    print("More")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
